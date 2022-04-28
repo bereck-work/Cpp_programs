6e977393501212a111449b64d131fa2cc965fbbc
KortaPo <70193867+KortaPo@users.noreply.github.com>
2022-04-28 20:00:52 +0530

    Updates

--- launcher.py
+++ launcher.py
@@ -1,11 +1,10 @@
-import questionary
-from src.process import Process
+from src.interactive import Interactive
 
 if __name__ == "__main__":
-    compiler = Process(
+    interactive = Interactive(
         compiler="g++",
-        cpp_compiler_flags="-pedantic -Wall -Wextra -std=c++17",
-        file_format=".cpp",
+        compiler_flags="-std=c++17",
+        format_of_the_file=".cpp",
+        cpp_source_directory="cpp",
     )
-    path = questionary.path("Enter the path of the file to compile: ").ask()
-    compiler.run_program(f"{path}")
+    interactive.run()
