593f38991867630da6aa1a5c3df61e9f424c5a38
KortaPo <70193867+KortaPo@users.noreply.github.com>
2022-07-01 00:00:15 +0530

    Create complement.cpp

---
+++ cpp/algorithms/complement.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using std::cout, std::cin, std::endl;
+
+
+// Returns '0' for '1' and '1' for '0'
+char flip(char c) {return (c == '0')? '1': '0';}
+ 
+// Print 1's and 2's complement of binary number, the parameter bin takes a string
+void printOneAndTwosComplement(string bin)
+{
+    int n = bin.length();
+    int i;
+ 
+    string ones, twos;
+    ones = twos = "";
+ 
+    //  for ones complement flip every bit
+    for (i = 0; i < n; i++)
+        ones += flip(bin[i]);
+ 
+    //  for two's complement go from right to left in
+    //  ones complement and if we get 1 make, we make
+    //  them 0 and keep going left when we get first
+    //  0, make that 1 and go out of loop
+    twos = ones;
+    for (i = n - 1; i >= 0; i--)
+    {
+        if (ones[i] == '1')
+            twos[i] = '0';
+        else
+        {
+            twos[i] = '1';
+            break;
+        }
+    }
+ 
+    // If No break : all are 1  as in 111  or  11111;
+    // in such case, add extra 1 at beginning
+    if (i == -1)
+        twos = '1' + twos;
+ 
+ 
+    cout << "1's complement: " << ones << endl;
+    cout << "2's complement: " << twos << endl;
+}
+ 
+
+int main()
+{
+    string bin = "1100";
+    printOneAndTwosComplement(bin);
+    return 0;
+}
