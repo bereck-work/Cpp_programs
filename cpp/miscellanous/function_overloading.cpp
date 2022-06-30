1331c6695e66c5591cd5f5d567eb12b517981543
KortaPo <70193867+KortaPo@users.noreply.github.com>
2022-06-30 23:42:02 +0530

    Create function_overloading.cpp

---
+++ cpp/miscellanous/function_overloading.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using std::cout, std::endl;
+
+// function with float type parameter
+float absolute(float var){
+    if (var < 0.0)
+        var = -var;
+    return var;
+}
+
+// function with int type parameter
+int absolute(int var) {
+     if (var < 0)
+         var = -var;
+    return var;
+}
+
+int main() {
+    
+    // call function with int type parameter
+    cout << "Absolute value of -5 = " << absolute(-5) << endl;
+
+    // call function with float type parameter
+    cout << "Absolute value of 5.5 = " << absolute(5.5f) << endl;
+    return 0;
+}
