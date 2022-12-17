2937865023850131174447728a107ceaee797c38
KortaPo <70193867+KortaPo@users.noreply.github.com>
2022-12-17 22:07:46 +0530

    Removed usage of std::endl and used "\n"

--- cpp/algorithms/fibonnaci.cpp
+++ cpp/algorithms/fibonnaci.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-using std::cout, std::endl, std::cin;
+using std::cout, std::cin;
 
 
 int fibonacci(int n)
@@ -9,13 +9,13 @@ int fibonacci(int n)
     if (n <= 1)
         return n;
     return fibonacci(n-1) + fibonacci(n-2);
-}
+};
 
 // Main program
 int main(){
     int a = 0;
-    cout << "Enter a number that you want to find Fibonacci of: " << endl;
+    cout << "Enter a number that you want to find Fibonacci of: " << "\n";
     cin >> a;
-    cout << fibonacci(a) << endl;
+    cout << fibonacci(a) << "\n";
     return 0;
 }
