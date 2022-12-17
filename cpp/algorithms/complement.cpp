db0024e8c5bdc3fa3b3337d262e342fa83f4ed4d
KortaPo <70193867+KortaPo@users.noreply.github.com>
2022-12-17 22:04:48 +0530

    Used "\n" instead of std::endl

--- cpp/algorithms/complement.cpp
+++ cpp/algorithms/complement.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
-using std::cout, std::cin, std::endl;
+using std::cout, std::cin, std::string;
 
 
 // Returns '0' for '1' and '1' for '0'
 char flip(char c) {return (c == '0')? '1': '0';}
- 
+
 // Print 1's and 2's complement of binary number, the parameter bin takes a string
 void printOneAndTwosComplement(string bin)
 {
     int n = bin.length();
     int i;
- 
+
     string ones, twos;
     ones = twos = "";
- 
+
     //  for ones complement flip every bit
     for (i = 0; i < n; i++)
         ones += flip(bin[i]);
- 
+
     //  for two's complement go from right to left in
     //  ones complement and if we get 1 make, we make
     //  them 0 and keep going left when we get first
@@ -33,17 +33,17 @@ void printOneAndTwosComplement(string bin)
             break;
         }
     }
- 
+
     // If No break : all are 1  as in 111  or  11111;
     // in such case, add extra 1 at beginning
     if (i == -1)
         twos = '1' + twos;
- 
- 
-    cout << "1's complement: " << ones << endl;
-    cout << "2's complement: " << twos << endl;
+
+
+    cout << "1's complement: " << ones << "\n";
+    cout << "2's complement: " << twos << "\n";
 }
- 
+
 
 int main()
 {
